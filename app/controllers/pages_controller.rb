18c333e57ff02dbf5d7c829c56d146b7e6fe2312
Khijbar Qayoumi <khijbar@gmail.com>
2016-05-29 22:49:30 +0200

    added jquery to top of app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -24,6 +24,24 @@ class PagesController < ApplicationController
   def faq
   end
 
+  def faq_about_our_services
+  end
+
+  def faq_contact_booster
+  end
+
+  def faq_boosting_process
+  end
+
+  def faq_payment
+  end
+
+  def faq_safety_security
+  end
+
+  def faq_about_elo_boost
+  end
+
   def contact
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   get 'boosting'            =>  'pages#boosting'
   get 'boosters'            =>  'pages#boosters'
   get 'faqs'                =>  'pages#faq'
+  get 'faqs_boost'          =>  'pages#faq_about_elo_boost'
+  get 'faqs_services'       =>  'pages#faq_about_our_services'
+  get 'faqs_process'        =>  'pages#faq_boosting_process'
+  get 'faqs_contact'        =>  'pages#faq_contact_booster'
+  get 'faqs_payment'        =>  'pages#faq_payment'
+  get 'faqs_safety'         =>  'pages#faq_safety_security'
   get 'contact'             =>  'pages#contact'
   get 'elo-boost'           =>  'pages#elo_boost'
   get 'division-boost'      =>  'pages#division_boost'
@@ -13,7 +19,6 @@ Rails.application.routes.draw do
   get 'placement-matches'   =>  'pages#placement_matches'
   get 'normal-draft'        =>  'pages#normal_draft'
   get 'budget-boosting'     =>  'pages#budget_boosting'
-
 
 # https://intense-harbor-93889.herokuapp.com/ | https://git.heroku.com/intense-harbor-93889.git
 
