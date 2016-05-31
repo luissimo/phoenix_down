8b6c9500c0f98323e5fe417e06b20660285f1a83
Khijbar Qayoumi <khijbar@gmail.com>
2016-05-31 18:26:13 +0200

    resetted devise views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,19 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings =
+      {
+
+          :address            => 'smtp.gmail.com',
+          :port               => 587,
+          :domain             => 'gmail.com', #you can also use google.com
+          :authentication     => :plain,
+          :user_name          => 'khijbar@gmail.com',
+          :password           => 'jlmabc321'
+      }
 
   config.cache_classes = false
 
