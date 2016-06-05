d7fb37e60a28b010ec1f5ca1394c4c2b3f5ec08c
Khijbar Qayoumi <khijbar@gmail.com>
2016-06-05 13:42:57 +0200

    design pimp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 
 
-
   def full_title(page_title = '')
     base_title = "Leauge of Legends ELO Boosting"
     if page_title.empty?
@@ -10,7 +9,6 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
-
 
   def resource_name
     :user
@@ -24,5 +22,18 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
+  def resource_contact_new
+    @boost_form = Contact.new
+  end
 
+  def resource_contact_create
+    @boost_form = Contact.new(params[:contact])
+    @boost_form.request = request
+    if @boost_form.deliver
+      flash.now[:notice] = "Thank you very much, we will contact you on your email with further instructions"
+    else
+      flash.now[:error]  = "Please fill in valid details"
+      render 'devise/registrations/edit'
+    end
+  end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,20 +1,19 @@
 class Contact < MailForm::Base
-  attribute :paypal_id,              :validate => true
-  attribute :email,                  :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :paypal_id,                      :validate => true
+  attribute :email,                          :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
-  attribute :account_name,           :validate => true
-  attribute :password,               :validate => true
-  attribute :password_confirmation,  :validate => true
-  attribute :nickname,               :captcha  => true
+  attribute :account_name,                   :validate => true
+  attribute :account_password,               :validate => true
+  attribute :account_password_confirmation,  :validate => true
+  attribute :nickname,                       :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
     {
         :subject => "Boost form filled in",
-        :to => "afg.010@hotmail.com",
-        :from => %("#{paypal_id}" <#{email}>)
+        :to => "afg.010@hotmail.com, khijbar@gmail.com",
+        :from => %("#{account_name}" <#{email}>)
     }
   end
-
 end
