31a59690929493821aacd468a50de4963ad06d7c
Khijbar Qayoumi <khijbar@gmail.com>
2016-06-16 15:53:01 +0200

    host production changed / no https://

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
     ## host for production devise
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'https://intense-harbor-93889.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'intense-harbor-93889.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
